2e27ea445923fba88998ebf26f5f1cda4767786a
Joel Alcántara Burgos <alcantara0729@gmail.com>
2025-03-11 22:23:26 -0400

    Initial Commit

---
+++ TVMazeApp/TVMazeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeAppApp.swift
+//  TVMazeApp
+//
+//  Created by Joel Alcantara on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TVMazeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
